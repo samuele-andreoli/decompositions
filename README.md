468b086e20db74c975ca355143019ffeb38261b1
Samuele Andreoli <samuele.andreoli@yahoo.it>
2024-05-16 12:15:27 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Decompositions using 
+
+This repository contains two SageMath notebooks to recompute all data used in writing [1].
+
+# References
+
+[1] S. Andreoli, G. Petrides, "Further Existence Results of Decompositions of Permutation Polynomials"l. Unpublished
---
+++ core_utils.py
@@ -0,0 +1,39 @@
+def dyadic(n):
+    i = 0
+
+    while n % 2 == 0:
+        n = n>>1
+        i += 1
+
+    return i
+
+
+def _order_prime(a, p, pm1_divisors):
+    for d in pm1_divisors:
+        if pow(a, d, p) == 1:
+            return d
+    
+    # Should never get here
+    return p-1
+
+
+def _order_primepower(a, p, e, pm1_divisors):
+    n = p
+    o = _order_prime(a % n, n, pm1_divisors)
+
+    for _ in range(1,e):
+        n = p * n
+    
+        if pow(a % n, o, n) != 1:
+            o = p * o
+    
+    return o
+
+
+def order(a, factors, divisors):
+    o = 1
+    for p, D in zip(factors, divisors):
+        o_mod_p = _order_primepower(a,p[0],p[1],D)
+        o = lcm(o,o_mod_p)
+    
+    return o
